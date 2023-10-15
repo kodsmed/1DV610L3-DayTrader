9a4bd3e8b1f8298f9a699210f013ee8fb2afa0a2
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-10-15 20:51:49 +0200

    start getting the pieces together

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import './gitSubModules/graphModule/components/graphdrawer/graphdrawer.js'
 import jk224jvGraphdrawer from './gitSubModules/graphModule/components/graphdrawer/graphdrawer.js'
 // NOTE: The apparent double import is deliberate. The first import declares the custom element, the second import is for the type definition. Removing either import will cause problems with the compiler/IDE respectively.
+import { TimeConverter } from './classes/TimeConverter.js'
+import { RawApiData } from './classes/RawApiData.js'
+import { ApiDataParser } from './classes/ApiDataParser.js'
+import { Stock } from './classes/Stock.js'
+import { ControlsView } from './classes/view/ControlsView.js'
 
 const graphComponent = document.createElement('jk224jv-graphdrawer') as jk224jvGraphdrawer
 const container = document.querySelector('#component')
@@ -8,21 +13,40 @@ if (container) {
   container.appendChild(graphComponent)
 }
 graphComponent.setAxisTitles({xAxis: 'Month and Day', yAxis: 'Value in $'})
-graphComponent.setXAxisLabels(['a', 'b', 'c', 'd', 'e'])
-graphComponent.renderArrayAsGraph([1,3,4,5,67])
+getStockHistory()
 
-
-
+const controlsView = new ControlsView()
 
 async function getStockHistory() {
-  const response = await fetch ('http://localhost:8080/api/stocks/APPLE?from=2019-01-01&to=2019-01-02', {
+  const ticker = 'AAPL'
+  const timeConverter = new TimeConverter()
+  const now = new Date()
+  const oneYearAgo = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate())
+  const from = timeConverter.convertDateToUnixTime(oneYearAgo)
+  const to = timeConverter.convertDateToUnixTime(now)
+  const response = await fetch (`http://localhost:8080/api/stocks/${ticker}?from=${from}&to=${to}`, {
     method: 'GET'
   })
 
-  console.log(response.body)
-  const p = document.createElement('p')
-  p.innerText = await response.text()
-  document.body.appendChild(p)
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  if (response.body === null) {
+    throw new Error(`Response body is null`)
+  }
+
+  const rawData = await JSON.parse(await response.text()) as RawApiData
+
+  const parser = new ApiDataParser()
+  const stock = parser.parseToStock(rawData) as Stock
+
+  const graphComponent = document.querySelector('jk224jv-graphdrawer') as jk224jvGraphdrawer
+
+  graphComponent.setXAxisLabels(stock.datesArray)
+  console.log(stock.valueArray)
+  graphComponent.renderArrayAsGraph(stock.valueArray)
+  console.log(stock.valueArray)
+
 }
 
 const containerElement = document.querySelector("#componentBox")
@@ -31,5 +55,5 @@ if (containerElement) {
   containerElement.appendChild(drawer)
 }
 
-getStockHistory()
+
 
