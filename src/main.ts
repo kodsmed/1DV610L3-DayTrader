b973561633b2f741176bb29c95216e60f578dd09
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-10-20 13:14:24 +0200

    add crude restart errorhandling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './gitSubModules/graphModule/components/graphdrawer/graphdrawer.js'
 import jk224jvGraphdrawer from './gitSubModules/graphModule/components/graphdrawer/graphdrawer.js'
-// NOTE: The apparent double import is deliberate. The first import declares the custom element, the second import is for the type definition. Removing either import will cause problems with the compiler/IDE respectively.
+// NOTE: The apparent double import above is deliberate. The first import declares the custom element, the second import is for the type definition. Removing either import will cause problems with the compiler/IDE respectively even if IDE claims the second is un-used.
 import { ApiCaller } from './classes/utilities/ApiCaller.js'
 import { GameController } from './classes/controller/GameController.js'
+import { Stock } from './classes/model/Stock.js'
 
 const graphComponent = document.createElement('jk224jv-graphdrawer') as any
 const container = document.querySelector('#component')
@@ -14,5 +15,14 @@ graphComponent.setAxisTitles({xAxis: 'Month and Day', yAxis: 'Value in $'})
 const apiCaller = new ApiCaller()
 const stocks = await apiCaller.getStockHistories()
 
-const gameController = new GameController(stocks)
-gameController.startGame()
+function runGame(stocks: Stock[]) {
+  try {
+    const gameController = new GameController(stocks)
+    gameController.startGame()
+  }
+  catch (error) {
+    runGame(stocks)
+  }
+}
+
+runGame(stocks)
