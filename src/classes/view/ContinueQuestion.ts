8efc470370ca48132a3d9b332c667a29d0be758f
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-10-21 22:39:57 +0200

    fix vertial ordering

--- src/classes/view/ContinueQuestion.ts
+++ src/classes/view/ContinueQuestion.ts
@@ -1,15 +1,22 @@
 export class ContinueQuestions {
   continueScreen: any
 
-  #createStyledElement(tag: string, className: string, textContent = '') {
-    const element = document.createElement(tag)
-    if (className) {
-      element.className = className
-    }
-    if (textContent != '') {
-      element.textContent = textContent
-    }
-    return element
+  displayContinueQuestion() {
+    this.continueScreen = this.#createStyledElement('div', 'continue-background')
+
+    const questionDiv = this.#createStyledElement('div', 'continue-content')
+    const questionText = this.#createStyledElement('h1', '', 'There is a save, would you like to continue?')
+
+    const buttonDiv = this.#createStyledElement('div', 'button-div')
+    const yesButton = this.#createButton('Yes, load save', 'green-button', 'continueGame')
+    const noButton = this.#createButton('No, start over', 'red-button', 'playAgain')
+
+    buttonDiv.appendChild(yesButton)
+    buttonDiv.appendChild(noButton)
+    questionDiv.appendChild(questionText)
+    questionDiv.appendChild(buttonDiv)
+    this.continueScreen.appendChild(questionDiv)
+    document.body.appendChild(this.continueScreen)
   }
 
   #createButton(text: string, colorClassName: string, clickEventName: string) {
@@ -22,21 +29,14 @@ export class ContinueQuestions {
     return button
   }
 
-  displayContinueQuestion() {
-    this.continueScreen = this.#createStyledElement('div', 'continue-background');
-
-    const questionDiv = this.#createStyledElement('div', 'continue-content');
-    const questionText = this.#createStyledElement('h1', '', 'There is a save, would you like to continue?');
-
-    const buttonDiv = this.#createStyledElement('div', 'button-div');
-    const yesButton = this.#createButton('Yes, load save', 'green-button', 'continueGame');
-    const noButton = this.#createButton('No, start over', 'red-button', 'playAgain');
-
-    buttonDiv.appendChild(yesButton)
-    buttonDiv.appendChild(noButton)
-    questionDiv.appendChild(questionText)
-    questionDiv.appendChild(buttonDiv)
-    this.continueScreen.appendChild(questionDiv)
-    document.body.appendChild(this.continueScreen)
+  #createStyledElement(tag: string, className: string, textContent = '') {
+    const element = document.createElement(tag)
+    if (className) {
+      element.className = className
+    }
+    if (textContent != '') {
+      element.textContent = textContent
+    }
+    return element
   }
 }
