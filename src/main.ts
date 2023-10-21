c5f84505bbdd547f4cd21ea68984cdcccf27db9b
Jimmy Karlsson <jk224jv@student.lnu.se>
2023-10-21 16:37:34 +0200

    allow the game to crash and show error instead of silent restart

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import jk224jvGraphdrawer  from 'graphdrawer/type'
 // NOTE: The apparent double import above is deliberate. The first import declares the custom element, the second import is for the type definition.
 import { ApiCaller } from './classes/utilities/ApiCaller.js'
 import { GameController } from './classes/controller/GameController.js'
-import { Stock } from './classes/model/Stock.js'
+import { MessageRenderer } from './classes/view/MessageRenderer.js'
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -20,16 +20,20 @@ if (container) {
 
 graphComponent.setAxisTitles({xAxis: 'Month and Day', yAxis: 'Value in $'})
 const apiCaller = new ApiCaller()
-const stocks = await apiCaller.getStockHistories()
 
-function runGame(stocks: Stock[]) {
+
+async function runGame() {
   try {
+    const stocks = await apiCaller.getStockHistories()
     const gameController = new GameController(stocks)
     gameController.startGame()
   }
   catch (error) {
-    runGame(stocks)
+    const messageRenderer = new MessageRenderer()
+    const errorMessage = "Something went wrong. Please refresh the page and try again."
+    // Display the error message then do nothing
+    messageRenderer.showWMessageForSecondsBeforeCallback(errorMessage, 0, () => {})
   }
 }
 
-runGame(stocks)
+runGame()
